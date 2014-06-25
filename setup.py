c7ffe534f430422716ff40c84629221220e94dae
Ian Good <ian.good@rackspace.com>
2014-06-25 16:57:59 -0400

    set version 0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='mygrate',
-      version='0.4',
+      version='0.5',
       author='Ian Good',
       author_email='ian.good@rackspace.com',
       description='Facilitates the migration of data from MySQL or MariaDB with no downtime.',
